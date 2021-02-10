6faa8eb70b0739b07ff1b435ea2a9c3029de49ad
jlaqueyrie <jerome.laqueyrie@hotmail.com>
2021-02-10 15:48:43 +0100

    ajout du controler model vue pour les posts

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,13 +1,14 @@
-<?php require APPROOT.'\views\inc\header.php'?>
+<?php require APPROOT . '\views\inc\header.php' ?>
 
 <div class="row">
     <div class="col-md-6">
-        <h1>Articles</h1> 
+        <h1>Articles</h1>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-8 ml-3">
         <a href="" class="btn btn-info pull-right">
             <i class="fas fa-pencil-alt"></i>
+            ajouter un article
         </a>
     </div>
 </div>
-<?php require APPROOT.'\views\inc\footer.php'?>
+<?php require APPROOT . '\views\inc\footer.php' ?>
